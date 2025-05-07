7558997ee1cb269487f92a58aecdff324ea6f57e
Hyeyoung Moon <mummhy0811@gmail.com>
2025-05-07 17:43:13 +0900

    feat: 일기 그룹 CRUD API 개발(#4)

---
+++ src/main/java/com/kernel/_SenseLog/group/dto/req/CreateGroupReqDto.java
@@ -0,0 +1,9 @@
+package com.kernel._SenseLog.group.dto.req;
+
+import lombok.Getter;
+
+@Getter
+public class CreateGroupReqDto {
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/kernel/_SenseLog/group/repository/GroupRepository.java
@@ -0,0 +1,7 @@
+package com.kernel._SenseLog.group.repository;
+
+import com.kernel._SenseLog.group.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
