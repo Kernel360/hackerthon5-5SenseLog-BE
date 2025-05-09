9ebaaddbace3b2c2bab3ae4b76e0e7c805c3e341
Yeju-Kang <bbogaeme@github.com>
2025-05-09 15:25:57 +0900

    fix : URL 수정

--- src/main/java/com/kernel/sense_log/config/WebConfig.java
+++ src/main/java/com/kernel/sense_log/config/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://deploy-react-1:3000")
                 .allowedMethods("*")
                 .allowCredentials(true)
                 .allowedHeaders("*");
